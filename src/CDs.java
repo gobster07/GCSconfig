729ad745b7b80c98cb914364b786eea364f58a8d
esgarbossa <enzosgarbossa09@gmail.com>
2026-08-28 18:41:09 -0300

    bora

--- src/CDs.java
+++ src/CDs.java
@@ -1,4 +1,17 @@
-package PACKAGE_NAME;
+import java.util.List;
 
-public class CDs {
+public class CDs extends Item{
+
+    private String Genre;
+    List<String> tracklist;
+
+    public CDs(int id, String title, String genre, List<String> tracklist) {
+        super(id, title);
+        Genre = genre;
+        this.tracklist = tracklist;
+    }
+
+    public void AddTracks(String song){
+        tracklist.add(song);
+    }
 }
--- src/DVDs.java
+++ src/DVDs.java
@@ -1,4 +1,16 @@
-package PACKAGE_NAME;
+public class DVDs extends Item{
 
-public class DVDs {
+private String typeOf;
+private String description;
+
+    public DVDs(int id, String title, String typeOf, String description) {
+        super(id, title);
+        this.typeOf = typeOf;
+        this.description = description;
+    }
+
+    public DVDs(String typeOf, String description) {
+        this.typeOf = typeOf;
+        this.description = description;
+    }
 }
