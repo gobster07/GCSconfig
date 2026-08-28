0d2ff0c29c6db9666a06c346c32348c5cf4ff13f
gobster07 <gobster07@gmail.com>
2026-08-28 18:14:54 -0300

    Start

---
+++ src/Item.java
@@ -0,0 +1,30 @@
+public class Item {
+    private int id;
+    private String title;
+
+    public Item (int id, String title){
+        this.id = id;
+        this.title = title;
+    }
+
+    public Item () {
+        this.id = -1;
+        this.title = "";
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+}
